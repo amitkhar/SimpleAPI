cc735d63ba030be366bc653c067fdc4edc2f8943
amitkhar <amitk.kharwar@outlook.com>
2021-06-26 23:49:53 +0530

    phase1

---
+++ Test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    
+    public class UnitTest1
+    {
+        WeatherForecastController wController = new WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+            //Act
+          var output =  wController.FetchData(1);
+          //Assert
+          Assert.Equal("Amit", output);
+        }
+    }
+}
